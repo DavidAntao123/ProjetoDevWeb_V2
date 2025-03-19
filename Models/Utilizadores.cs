a38e9832fc181dffdd9a2df61eca894457c96800
David Antao <david.antao@hotmail.com>
2025-03-19 10:23:58 +0000

    teste

---
+++ Models/Utilizadores.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjetoDevWeb_V2.Models;
+
+public class Utilizadores
+{
+    public int Id { get; set; }
+    
+    
+    public string Nome { get; set; }
+    public string Email { get; set; }
+    public string Password {get; set;}
+    
+}
